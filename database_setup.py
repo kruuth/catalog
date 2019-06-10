67388427d0cc83efcc9036fdc3a5c85be387a1ab
kruuth <iscariot@ix.netcom.com>
2019-06-10 18:25:48 -0400

    Update database_setup.py
    Add postgres data

--- database_setup.py
+++ database_setup.py
@@ -64,5 +64,6 @@ class Item(Base):
         }
 
 
-engine = create_engine('sqlite:///vgcatalog.db')
+#engine = create_engine('sqlite:///vgcatalog.db')
+engine = create_engine('postgresql://catalog:catalog@localhost/vgcatalog')
 Base.metadata.create_all(engine)
